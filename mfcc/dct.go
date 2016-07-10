c2f19da901a4bd07f77931ea0f74eb9b93adfa8e
Alex Nichol <unixpickle@gmail.com>
2016-07-10 15:43:14 -0400

    speed up DCT using trig IDs

--- mfcc/dct.go
+++ mfcc/dct.go
@@ -8,8 +8,19 @@ func dct(signal []float64, n int) []float64 {
 	res := make([]float64, n)
 	baseFreq := math.Pi / float64(len(signal))
 	for k := 0; k < n; k++ {
-		for i, x := range signal {
-			res[k] += x * math.Cos(baseFreq*float64(k)*(float64(i)+0.5))
+		initArg := baseFreq * float64(k) * 0.5
+		curCos := math.Cos(initArg)
+		curSin := math.Sin(initArg)
+
+		// Double angle formulas to avoid more sin and cos.
+		addCos := curCos*curCos - curSin*curSin
+		addSin := 2 * curCos * curSin
+
+		for _, x := range signal {
+			res[k] += x * curCos
+			// Angle sum formulas are a lot faster than
+			// recomputing sines and cosines.
+			curCos, curSin = curCos*addCos-curSin*addSin, curCos*addSin+addCos*curSin
 		}
 	}
 	return res
