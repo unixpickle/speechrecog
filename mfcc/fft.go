30ce5768cd9b685974d7e00a9bf295b3070f0d72
Alex Nichol <unixpickle@gmail.com>
2016-07-10 14:02:42 -0400

    optimized FFT
    the benchmark which used to take 215 microseconds now takes 30 microseconds. This compares favorably to https://github.com/mjibson/go-dsp, which I benchmarked and found to take 42 microseconds on the same benchmark (with one thread; multithreads actually makes it slower).

--- mfcc/fft.go
+++ mfcc/fft.go
@@ -18,6 +18,29 @@ type fftBins struct {
 // fft computes dot products of the signal with
 // various sinusoids.
 func fft(signal []float64) fftBins {
+	temp := make([]float64, len(signal))
+	signalCopy := make([]float64, len(signal))
+	copy(signalCopy, signal)
+
+	basePeriod := 2 * math.Pi / float64(len(signal))
+	sines := make([]float64, len(signal)/4)
+	cosines := make([]float64, len(signal)/4)
+	for i := range cosines {
+		if i < 2 || i%100 == 0 {
+			cosines[i] = math.Cos(basePeriod * float64(i))
+			sines[i] = math.Sin(basePeriod * float64(i))
+		} else {
+			// Angle sum formulas for cosine and sine.
+			cosines[i] = cosines[i-1]*cosines[1] - sines[i-1]*sines[1]
+			sines[i] = sines[i-1]*cosines[1] + cosines[i-1]*sines[1]
+		}
+	}
+
+	return destructiveFFT(signalCopy, temp, sines, cosines, 0)
+}
+
+func destructiveFFT(signal []float64, temp []float64, sines, cosines []float64,
+	depth uint) fftBins {
 	n := len(signal)
 	if n == 1 {
 		return fftBins{Cos: []float64{signal[0]}}
@@ -29,46 +52,36 @@ func fft(signal []float64) fftBins {
 		panic("input must be a power of 2")
 	}
 
-	halfSignal := make([]float64, len(signal)/2)
+	evenSignal := temp[:n/2]
+	oddSignal := temp[n/2:]
 	for i := 0; i < n; i += 2 {
-		halfSignal[i>>1] = signal[i]
+		evenSignal[i>>1] = signal[i]
+		oddSignal[i>>1] = signal[i+1]
 	}
-	evenBins := fft(halfSignal)
-
-	for i := 1; i < n; i += 2 {
-		halfSignal[i>>1] = signal[i]
-	}
-	oddBins := fft(halfSignal)
-
-	basePeriod := 2 * math.Pi / float64(n)
+	evenBins := destructiveFFT(evenSignal, signal[:n/2], sines, cosines, depth+1)
+	oddBins := destructiveFFT(oddSignal, signal[n/2:], sines, cosines, depth+1)
 
 	res := fftBins{
-		Cos: make([]float64, n/2+1),
-		Sin: make([]float64, n/2-1),
+		Cos: temp[:n/2+1],
+		Sin: temp[n/2+1:],
 	}
 
 	res.Cos[0] = evenBins.Cos[0] + oddBins.Cos[0]
-	for i := 1; i < n/4; i++ {
-		res.Cos[i] = evenBins.Cos[i] + math.Cos(basePeriod*float64(i))*oddBins.Cos[i] -
-			math.Sin(basePeriod*float64(i))*oddBins.Sin[i-1]
-	}
+	res.Cos[n/2] = evenBins.Cos[0] - oddBins.Cos[0]
 	res.Cos[n/4] = evenBins.Cos[n/4]
 	for i := 1; i < n/4; i++ {
-		j := n/4 - i
-		res.Cos[n/4+i] = evenBins.Cos[j] - math.Cos(basePeriod*float64(j))*oddBins.Cos[j] +
-			math.Sin(basePeriod*float64(j))*oddBins.Sin[j-1]
+		oddPart := cosines[i<<depth]*oddBins.Cos[i] -
+			sines[i<<depth]*oddBins.Sin[i-1]
+		res.Cos[i] = evenBins.Cos[i] + oddPart
+		res.Cos[n/2-i] = evenBins.Cos[i] - oddPart
 	}
-	res.Cos[n/2] = evenBins.Cos[0] - oddBins.Cos[0]
 
-	for i := 0; i < n/4-1; i++ {
-		res.Sin[i] = evenBins.Sin[i] + math.Sin(basePeriod*float64(i+1))*oddBins.Cos[i+1] +
-			math.Cos(basePeriod*float64(i+1))*oddBins.Sin[i]
-	}
 	res.Sin[n/4-1] = oddBins.Cos[len(oddBins.Cos)-1]
 	for i := 0; i < n/4-1; i++ {
-		j := n/4 - (i + 2)
-		res.Sin[i+n/4] = -evenBins.Sin[j] + math.Sin(basePeriod*float64(j+1))*oddBins.Cos[j+1] +
-			math.Cos(basePeriod*float64(j+1))*oddBins.Sin[j]
+		oddPart := sines[(i+1)<<depth]*oddBins.Cos[i+1] +
+			cosines[(i+1)<<depth]*oddBins.Sin[i]
+		res.Sin[i] = evenBins.Sin[i] + oddPart
+		res.Sin[len(res.Sin)-(i+1)] = -evenBins.Sin[i] + oddPart
 	}
 
 	return res
